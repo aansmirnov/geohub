57ac4bbe5b3ca45f42b35c69d1d89e3f51479e00
aansmirnov <aansmrnv@gmail.com>
2024-03-12 19:43:38 +0300

    feat(mobile): add hover effect for button

--- apps/mobile/App.tsx
+++ apps/mobile/App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import { Alert, StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { Button, Text } from 'src/components';
 import { COLORS_MAP, PEACH } from 'src/components/ui/colors';
 import { useLoadFonts } from 'src/hooks';
@@ -18,7 +18,7 @@ export default function App() {
         <View style={styles.container}>
             <SvgXml xml={GEOHUB_LOGO_WITH_TEXT} width={209} height={247} />
             <SvgXml xml={GEOHUB_LOGO_WITHOUT_TEXT} width={144} height={170} />
-            <Button onPress={() => Alert.alert('Hello!')} borderRadius={22}>
+            <Button borderRadius={22}>
                 <Text color='white' fontSize={26} fontFamily='Inter-Medium'>
                     Log In
                 </Text>
--- apps/mobile/src/components/ui/button/index.tsx
+++ apps/mobile/src/components/ui/button/index.tsx
@@ -1,6 +1,7 @@
-import { Pressable, StyleSheet } from 'react-native';
+import { Pressable, StyleSheet, View } from 'react-native';
 import { ButtonProps } from './types';
 import { COLORS_MAP, GREEN } from '../colors';
+import { useState } from 'react';
 
 export const Button = ({
     children,
@@ -9,27 +10,49 @@ export const Button = ({
     // size = 'l',
     ...props
 }: ButtonProps) => {
+    const [onHover, setOnHover] = useState(false);
     const backgroundColor = COLORS_MAP[bg];
     const buttonSize = { width: 320, height: 62 };
+    const buttonHoverStyles = {
+        transition: 'all 100ms ease',
+        transform: 'scale(1.02)',
+        shadowColor: 'black',
+        shadowOpacity: 0.1,
+    };
 
     return (
-        <Pressable
-            style={{
-                backgroundColor,
-                borderRadius,
-                ...buttonSize,
-                ...styles.button,
-            }}
-            {...props}
-        >
-            {children}
-        </Pressable>
+        <View style={styles.buttonWrapper}>
+            <Pressable
+                style={{
+                    backgroundColor,
+                    borderRadius,
+                    ...buttonSize,
+                    ...styles.button,
+                    ...(onHover ? buttonHoverStyles : {}),
+                }}
+                onMoveShouldSetResponder={() => true}
+                onResponderStart={() => setOnHover(true)}
+                onResponderEnd={() => setOnHover(false)}
+                {...props}
+            >
+                {children}
+            </Pressable>
+            {onHover && <View style={{ ...styles.overlay, borderRadius }} />}
+        </View>
     );
 };
 
 const styles = StyleSheet.create({
+    buttonWrapper: {
+        position: 'relative',
+    },
     button: {
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    overlay: {
+        ...StyleSheet.absoluteFillObject,
+        backgroundColor: 'rgba(0, 0, 0, 0.2)',
+        transform: 'scale(1.02)',
     },
 });
