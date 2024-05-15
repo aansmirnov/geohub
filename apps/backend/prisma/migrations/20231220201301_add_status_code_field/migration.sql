76218e3cb57b95ab337a023d67e84d92bdb51184
aansmirnov <aansmrnv@gmail.com>
2024-05-15 21:01:25 +0300

    chore(backend): remove all files

--- apps/backend/prisma/migrations/20231220201301_add_status_code_field/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "authCode" INTEGER;
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_authCode_key" ON "User"("authCode");
--- apps/backend/src/api/auth/index.ts
+++
@@ -1,4 +0,0 @@
-export * from './register';
-export * from './login';
-export * from './reset-password';
-export * from './check-auth-code';
